2d538de8d490c80cf89e836d3fa7d3b1acdb6188
Krzysi3k <global.takeover@o2.pl>
2021-07-22 22:06:39 +0200

    added .net

--- run_benchmarks.ps1
+++ run_benchmarks.ps1
@@ -1,18 +1,26 @@
 Set-Location $pwd.Path
-Remove-Item .\results.txt -Force -ErrorAction SilentlyContinue
-powershell.exe .\s.ps1 | Out-File .\results.txt -Append
-pwsh.exe .\s.ps1 | Out-File .\results.txt -Append
-python .\s.py | Out-File .\results.txt -Append
-lua53.exe .\s.lua | Out-File .\results.txt -Append
-pypy3 .\s.py | Out-File .\results.txt -Append
-go run .\s.go | Out-File .\results.txt -Append
-julia .\s.jl | Out-File .\results.txt -Append
+Remove-Item .\results.txt -Force
+$programs = [ordered]@{
+    'powershell 5.1' = 'powershell .\s.ps1'
+    'powershell 7.1' = 'pwsh.exe .\s.ps1'
+    'python' = 'python .\s.py'
+    'pypy' = 'pypy3 .\s.py'
+    'lua' = 'lua53 .\s.lua'
+    'go' = 'go run .\s.go'
+    'julia' = 'julia .\s.jl'
+    'dotnet' = 'dotnet run .\s.cs'
+}
+
+foreach($prog in $programs.Keys) {
+    Write-Verbose "running $prog workload..." -Verbose
+    Invoke-Expression $programs[$prog] | Out-File .\results.txt -Append
+}
 
 $content = Get-Content .\results.txt
-$languages = @("powershell 5.1", "powershell 7.1", "python", "lua", "pypy", "go", "julia")
+[System.Collections.ArrayList]$languages = $programs.Keys
 $arr = @()
 for ($i = 0; $i -lt $languages.Count; $i++) {
-    $arr += [PSCustomObject]@{
+    $arr += [PSCustomObject] @{
         language=$languages[$i]
         result=$content[$i].Replace('ms',' milliseconds')
     }
---
+++ s.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace benchmarks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            for (int i = 1; i < 10001; i++)
+            {
+                Silnia(15);
+                for (int x = 1; x < 10001; x++)
+                {
+                    if(x == 9876)
+                        break;
+                }
+            }
+            stopwatch.Stop();
+            long elapsed = stopwatch.ElapsedMilliseconds;
+            System.Console.WriteLine($"finished in: {elapsed} ms");
+        }
+
+        public static int Silnia(int n)
+        {
+            if (n > 1)
+            {
+                int result = Silnia(n-1);
+                return result * n;
+            }
+            return 1;
+        }
+    }
+}
